8d3cb5e0a1aad30d6257b4e975b607cc105c6d4f
Gary Pang <gary.pang@gmail.com>
2016-06-29 03:38:43 -0400

    Add zoom function to map
    Users can zoom in and out of map. They can also drag it. Based on code
    from my old blog post at
    http://www.gary-pang.com/web-design/2015/02/17/gas-prices-today-d3-js-map/

--- d3-mapping.js
+++ d3-mapping.js
@@ -11,15 +11,30 @@ var projection = d3.geo.albers()
 var path = d3.geo.path()
 			 .projection(projection);
 
-var svg = d3.select("body").append("svg")
+// Map zoom function
+var zoom = d3.behavior.zoom()
+                      .translate([0, 0])
+                      .scale(1)
+                      .scaleExtent([1, 10])
+                      .on("zoom", zoomed);
+
+function zoomed() {
+  svgMap.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+var svgContainer = d3.select("body").append("svg")
 			.attr("width", width)
-			.attr("height", height);
+			.attr("height", height)
+            // Calls zoom function
+            .call(zoom);
+
+var svgMap = svgContainer.append("g");
 
 d3.json("GBR_adm2_topojson.json", function(error, uk) {
 	
 	var subunits = topojson.feature(uk, uk.objects.GBR_adm2);
 	
-	svg.selectAll(".subunit")
+	svgMap.selectAll(".subunit")
 	   .data(topojson.feature(uk, uk.objects.GBR_adm2).features)
 	   .enter()
 	   .append("path")
@@ -44,7 +59,7 @@ d3.json("GBR_adm2_topojson.json", function(error, uk) {
 
        .on("mousemove", function(d) {
         
-       //Update tooltip position and value
+       // Update tooltip position and value
        d3.select("#tooltip")
        .style("top", (d3.event.pageY) + 20 + "px")
        .style("left", (d3.event.pageX) + 20 + "px")
@@ -57,13 +72,12 @@ d3.json("GBR_adm2_topojson.json", function(error, uk) {
        .select("#value2")
        .text(d.properties.NAME_1);
        
-       //Show tooltip
+       // Show tooltip
        d3.select("#tooltip").classed("hidden", false);
        })
     
-	   //Hide tooltip
+	   // Hide tooltip
        .on("mouseout", function() {
-       //Hide the tooltip
        d3.select("#tooltip").classed("hidden", true);
        });
 });
